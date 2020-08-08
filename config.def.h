55cc84602ce71402bb3b9cc2231c1b3ecc07615a
afreakk <laderud@hotmail.com>
2020-08-08 12:17:07 +0200

    like it is now

--- config.def.h
+++ config.def.h
@@ -4,14 +4,21 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"hack:pixelsize=18:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char bg0[]	= "#1d2021";
+static const char bg1[]	= "#282828";
+static const char bg2[]	= "#3c3836";
+static const char fg0[]	= "#ebdbb2";
+static const char fg1[]	= "#fbf1c7";
+static const char accent[]	= "#fabd2f";
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { fg1, bg1 },
+	[SchemeSel] = { accent, bg2 },
+	[SchemeOut] = { fg0, bg0 },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
