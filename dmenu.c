9c0de2513d05ac76b89130d52afd1f876543ced3
afreakk <laderud@hotmail.com>
2021-03-20 12:22:05 +0100

    avoid overflowing width measure

--- dmenu.c
+++ dmenu.c
@@ -89,10 +89,10 @@ calcoffsets(void)
 			break;
 }
 
-static int
+static long int
 max_textw(void)
 {
-	int len = 0;
+	long int len = 0;
 	for (struct item *item = items; item && item->text; item++)
 		len = MAX(TEXTW(item->text), len);
 	return len;
